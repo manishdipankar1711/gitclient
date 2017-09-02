0a74c15b2e2c8636092748a917b592922c769be2
manishdipankar1711 <manishdipankar1711@gmail.com>
2017-09-02 20:05:35 +0530

    second iteration commit

---
+++ js/repositoryComponent.js
@@ -0,0 +1,30 @@
+var RepositoryComponent = (function (RepositoryComponent) {
+
+	RepositoryComponent.getRepository = function(repName){
+		
+		var url = "https://api.github.com/search/repositories?q="+repName
+
+		$.ajax({
+		  url: url,
+		  dataType: "json",
+		  data : data,
+		  type : "GET",
+		  success: function (resp, XMLHttpRequest ){
+		      if( resp ) {
+		          callback(resp);
+		      }else {
+		        handleError();
+		      }
+		  },
+		  error : function(XMLHttpRequest, textStatus, errorThrown){
+		    handleError();
+		  }
+		});
+	}
+
+	paintRepositry = funnction(repName){
+
+	}
+
+	return RepositoryComponent;
+}(RepositoryComponent || {}));
